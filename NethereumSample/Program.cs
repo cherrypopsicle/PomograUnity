0603b042c3e164f711a6558864ba5b591083a730
Memo Khoury <majd.khoury93@gmail.com>
2021-04-15 20:02:32 -0400

    dir: initialized files

---
+++ NethereumSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+
+namespace NethereumSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
